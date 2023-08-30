d3fa322c09726df45cb0027ae8e0e9f3d9d3016d
cerqiest <66092565+cerqiest@users.noreply.github.com>
2023-08-30 12:11:19 -0400

    add information about the latest commit to the console

--- js/index.js
+++ js/index.js
@@ -1,13 +1,45 @@
 // Copyright (c) 2023 znci. All rights reserved.
 // Licensed under the MIT License.
 // See LICENSE in the project root for license information.
-// Version: 1.0.0
-
-let version = `v1.0.0`;
 
 const znciAscii = `███████╗███╗   ██╗ ██████╗██╗\n╚══███╔╝████╗  ██║██╔════╝██║\n  ███╔╝ ██╔██╗ ██║██║     ██║\n ███╔╝  ██║╚██╗██║██║     ██║\n███████╗██║ ╚████║╚██████╗██║\n╚══════╝╚═╝  ╚═══╝ ╚═════╝╚═╝`;
 console.log(
-  `%c${znciAscii}\n\n %cLoading znci website ${version}...`,
+  `%c${znciAscii}\n\n %cLoading znci website...`,
   "color: #ba34eb; font-family: monospace; font-size: 24px;",
   "color: #346eeb; font-family: monospace; font-size: 24px;"
 );
+
+fetch("https://api.github.com/repos/znci/new-web/commits/main")
+  .then((res) =>
+    res.json().then((json) => {
+      const commitHash = json.sha.slice(0, 7);
+      const commitDate = json.commit.author.date;
+      const commitAuthor = json.commit.author.name;
+      const commitMessage = json.commit.message;
+      const commitLink = `https://github.com/znci/new-web/commit/${json.sha}`;
+
+      const staticStyles =
+        "color: #ba34eb; font-family: monospace; font-size: 16px;";
+      const dynamicStyles =
+        "color: #346eeb; font-family: monospace; font-size: 16px;";
+
+      console.log(
+        `%cLast commit: %c${commitHash} %cby %c${commitAuthor} %con %c${commitDate} %c(%c${commitMessage}%c). %cSee %c${commitLink} %cfor the commit.`,
+        staticStyles,
+        dynamicStyles,
+        staticStyles,
+        dynamicStyles,
+        staticStyles,
+        dynamicStyles,
+        staticStyles,
+        dynamicStyles,
+        staticStyles,
+        dynamicStyles,
+        staticStyles,
+        dynamicStyles
+      );
+    })
+  )
+  .catch((err) => {
+    console.error(err);
+  });
