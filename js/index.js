1eedc9993f8f764ba782c94f18a8b8bd2803d5c4
cerq <66092565+cerqiest@users.noreply.github.com>
2023-12-22 10:30:52 -0500

    Update mobile device alert message

--- js/index.js
+++ js/index.js
@@ -67,7 +67,7 @@ const previouslyAcknowledgedMobileAlert =
 
 if (isMobile && !previouslyAcknowledgedMobileAlert) {
   alert(
-    "👋🏼 Hey! It looks like you're using a mobile device. Just so you know, the znci website might not look quite right on mobile devices and we're actively looking for ways to improve your experience. (Not using a mobile device and mistakenly received this alert? Please let us know via the contact form below!)"
+    "👋🏼 Hey! It looks like you're using a mobile device. Just so you know, the znci website might not look quite right on mobile devices and we're actively looking for ways to improve your experience. (Not using a mobile device? Please let us know via the contact form below!)"
   );
 
   localStorage.setItem("previouslyAcknowledgedMobileAlert", true);
