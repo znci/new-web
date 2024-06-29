22c1f5c4ca428bcc4562b3b3ba63eb0936df3c67
cerq <66092565+cerqiest@users.noreply.github.com>
2024-06-29 16:24:06 -0400

    form updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 import solidJs from "@astrojs/solid-js";
 
 import cloudflare from "@astrojs/cloudflare";
@@ -8,5 +8,16 @@ export default defineConfig({
   site: "https://znci.dev",
   integrations: [solidJs()],
   output: "hybrid",
-  adapter: cloudflare()
+  adapter: cloudflare(),
+
+  experimental: {
+    env: {
+      schema: {
+        WEBHOOK_URL: envField.string({
+          context: "server",
+          access: "secret",
+        }),
+      },
+    },
+  },
 });
