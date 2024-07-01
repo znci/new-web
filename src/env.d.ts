02b557cdb434519ab998800dae58cdd8913b8e65
cerq <66092565+cerqiest@users.noreply.github.com>
2024-07-01 18:14:56 -0400

    add kv

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,8 @@
 /// <reference path="../.astro/env.d.ts" />
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+type KVNamespace = import("@cloudflare/workers-types").KVNamespace;
+type ENV = {
+  BLOCKLIST_KV: KVNamespace;
+};
